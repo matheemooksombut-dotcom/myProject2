91658cf6b2a0d73bcc6ed203fb70bc033f1b8785
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-10-30 17:25:28 +0700

    updated

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
- function toggleMenu() {
-      const menu = document.querySelector(".menu");
-      const space = document.getElementById("space");
-      menu.classList.toggle("active");
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  const space = document.getElementById("space");
+  menu.classList.toggle("active");
 
-      // เปิด/ปิด display (เพื่อให้สอดคล้องกับ CSS เดิมของคุณ)
-      if (menu.style.display === "block") {
-        menu.style.display = "none";
-        
-      } else {
-        menu.style.display = "block";
-      }
-        if (menu.classList.contains("active")) {
-        space.style.marginTop = "200px";
-      } else {
-        space.style.marginTop = "0px";
-      }
-      }
-          
-    
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 
+  // ! จัพื้นที่ margin ในการ แสดง content
 
-  
+  if (menu.classList.contains("active")) {
+    space.style.marginTop = "200px";
+  } else {
+    space.style.marginTop = "0px";
+  }
+}

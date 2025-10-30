bb220098d50726c8c470ff69e56c572543ad2e2c
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-10-30 17:01:52 +0700

    updated

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
  function toggleMenu() {
       const menu = document.querySelector(".menu");
+      const space = document.getElementById("space");
       menu.classList.toggle("active");
+
       // เปิด/ปิด display (เพื่อให้สอดคล้องกับ CSS เดิมของคุณ)
       if (menu.style.display === "block") {
         menu.style.display = "none";
+        
       } else {
         menu.style.display = "block";
       }
-    }
+        if (menu.classList.contains("active")) {
+        space.style.marginTop = "200px";
+      } else {
+        space.style.marginTop = "0px";
+      }
+      }
+          
+    
+
+
+  
